f34bb3d9a929f0d84da9738ee55ba2e0aa8866e2
Sander Paulus <sander@Sanders-Laptop.local>
2023-12-01 19:35:07 +0100

    AOC2023 day 1 part 2

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,4 +1,4 @@
-import {CoordinateSanitizer, day1} from './day1';
+import {CoordinateSanitizer, day1, day1_part2} from './day1';
 
 describe('calibration detector', () => {
   it('detects the first and last number of each line, combines them', () => {
@@ -16,7 +16,40 @@ describe('calibration detector', () => {
      expect(actual).toEqual(11);
   });
 
+ it('combines the same digit twice of only one digit is present', () => {
+   const input = 'jjfvnnlfivejj1';
+     let actual = new CoordinateSanitizer().processLine(input);
+     console.log(actual)
+     expect(actual).toEqual(11);
+  });
+ it('detects the first and last number of each line, combines them', () => {
+    const input = '4jjfvnn6lfi7v8ejj1';
+    expect(new CoordinateSanitizer().processLineWithWords(input)).toEqual(41);
+  });
+  it('returns null if no number is present', () => {
+    const input = 'jjfvnnlfgivejj';
+    expect(new CoordinateSanitizer().processLineWithWords(input)).toEqual(NaN);
+  });
+ it('combines the same digit twice of only one digit is present', () => {
+   const input = 'jjfvnnlfigvejj1';
+     let actual = new CoordinateSanitizer().processLineWithWords(input);
+     console.log(actual)
+     expect(actual).toEqual(11);
+  });
+
+ it('can detect verbose digits is present', () => {
+   const input = 'jjfvnnlfivejj1';
+   console.log(input.includes("five"));
+     let actual = new CoordinateSanitizer().processLineWithWords(input);
+     console.log(actual)
+     expect(actual).toEqual(51);
+  });
+
  it('run day1', () => {
    console.log(day1());
+  });
+
+ it('run day1 - part 2', () => {
+   console.log(day1_part2());
   });
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -4,22 +4,110 @@ import {readFileLines} from "../file-reader";
 export function day1(): number {
     let fullyQualifiedFileName = join(__dirname, 'day1.txt');
     const sanitizer = new CoordinateSanitizer();
-    let sum = readFileLines(fullyQualifiedFileName)
+    return readFileLines(fullyQualifiedFileName)
         .map(line => sanitizer.processLine(line))
-        .filter(combinedDigits => !isNaN(combinedDigits) )
-        .reduce((prev, current) => prev + current)
-    return sum;
+        // .filter(combinedDigits => !isNaN(combinedDigits))
+        .reduce((prev, current) => prev + current);
 }
 
+export function day1_part2(): number {
+    let fullyQualifiedFileName = join(__dirname, 'day1.txt');
+    const sanitizer = new CoordinateSanitizer();
+    return readFileLines(fullyQualifiedFileName)
+        .map(line => sanitizer.processLineWithWords(line))
+        .reduce((prev, current) => prev + current,0);
+}
 
 export class CoordinateSanitizer {
     public processLine(input: String): number {
         let numbers = input.split("")
-            .filter(i => "0123456789".includes(i));
-        if(numbers.length == 0){
+            .filter(i => "123456789".includes(i));
+        if (numbers.length == 0) {
             return NaN;
         }
-        return parseInt(numbers[0] + numbers[numbers.length-1]);
+        return parseInt(numbers[0] + numbers[numbers.length - 1]);
     }
+
+    public processLineWithWords(input: String): number {
+        let firstDigit = this.findFirstDigit(input);
+        let lastDigit = this.findLastDigit(input);
+        let number = firstDigit! * 10 + lastDigit!;
+        console.log(`input ${input} -> number ${number}`)
+        return number
+    }
+
+    private findFirstDigit(input: String): number | undefined {
+        let digit
+        let verboseDigit = ""
+        let count = 0
+        while (!digit && count < input.length) {
+            let character = input[count]
+            let digit = this.checkDigit(character)
+            if (!digit) {
+                verboseDigit += character;
+                digit = this.checkVerboseDigit(verboseDigit)
+            }
+            if (digit) {
+                return digit;
+            } else {
+                count++
+            }
+        }
+    }
+
+    private findLastDigit(input: String): number | undefined {
+        let digit
+        let verboseDigit = ""
+        let count = input.length
+        while (!digit && count > 0) {
+            count--
+            let character = input[count]
+            let digit = this.checkDigit(character)
+            if (!digit) {
+                verboseDigit = character + verboseDigit;
+                digit = this.checkVerboseDigit(verboseDigit)
+            }
+            if (digit) {
+                return digit;
+            }
+        }
+    }
+
+    private checkDigit(character: string) {
+        if ("123456789".includes(character)) {
+            return parseInt(character)
+        }
+    }
+
+    private checkVerboseDigit(verboseDigit: string) {
+        if (verboseDigit.includes("one")) {
+            return 1;
+        }
+        if (verboseDigit.includes("two")){
+            return 2;
+        }
+        if (verboseDigit.includes("three")){
+            return 3;
+        }
+        if (verboseDigit.includes("four")){
+            return 4;
+        }
+        if (verboseDigit.includes("five")){
+            return 5;
+        }
+        if (verboseDigit.includes("six")){
+            return 6;
+        }
+        if (verboseDigit.includes("seven")){
+            return 7;
+        }
+        if (verboseDigit.includes("eight")){
+            return 8;
+        }
+        if (verboseDigit.includes("nine")){
+            return 9;
+        }
+    }
+
 }
 
