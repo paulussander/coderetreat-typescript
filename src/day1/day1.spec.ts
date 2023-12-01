70c1fdbe4ffc5da83e8316f11c38006fbca46cd2
Sander Paulus <sander@Sanders-Laptop.iconos.be>
2023-12-01 14:36:52 +0100

    AOC2023 day 1 part 1

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,9 +1,22 @@
-import { day1 } from './day1';
+import {CoordinateSanitizer, day1} from './day1';
 
-describe('Dummy test', () => {
-  it('this is test for the dummy function️️', () => {
-    const result = day1();
-    expect(result).toEqual('sayanora mofos');
+describe('calibration detector', () => {
+  it('detects the first and last number of each line, combines them', () => {
+    const input = '4jjfvnn6lfi7v8ejj1';
+    expect(new CoordinateSanitizer().processLine(input)).toEqual(41);
+  });
+  it('returns null if no number is present', () => {
+    const input = 'jjfvnnlfgivejj';
+    expect(new CoordinateSanitizer().processLine(input)).toEqual(NaN);
+  });
+ it('combines the same digit twice of only one digit is present', () => {
+   const input = 'jjfvnnlfigvejj1';
+     let actual = new CoordinateSanitizer().processLine(input);
+     console.log(actual)
+     expect(actual).toEqual(11);
   });
 
+ it('run day1', () => {
+   console.log(day1());
+  });
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,12 +1,25 @@
 import {join} from 'path';
 import {readFileLines} from "../file-reader";
 
-export function day1(input: string = 'sayanora mofos'): string {
+export function day1(): number {
     let fullyQualifiedFileName = join(__dirname, 'day1.txt');
-    readFileLines(fullyQualifiedFileName)
-        .forEach(line => console.log("line: " + line))
-    return `${input}`;
+    const sanitizer = new CoordinateSanitizer();
+    let sum = readFileLines(fullyQualifiedFileName)
+        .map(line => sanitizer.processLine(line))
+        .filter(combinedDigits => !isNaN(combinedDigits) )
+        .reduce((prev, current) => prev + current)
+    return sum;
 }
 
-console.log(day1());
+
+export class CoordinateSanitizer {
+    public processLine(input: String): number {
+        let numbers = input.split("")
+            .filter(i => "0123456789".includes(i));
+        if(numbers.length == 0){
+            return NaN;
+        }
+        return parseInt(numbers[0] + numbers[numbers.length-1]);
+    }
+}
 
