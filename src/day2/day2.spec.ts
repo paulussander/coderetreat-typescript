79b75477deb34b8d2a971fb7c030556c3016a4d0
Sander Paulus <sander@Sanders-Laptop.local>
2023-12-02 15:02:56 +0100

    AOC2023 day 2 part 1

---
+++ src/day2/day2.spec.ts
@@ -0,0 +1,11 @@
+import {day2, day2_part2} from './day2';
+
+describe('calibration detector', () => {
+    it('run day2', () => {
+        console.log(day2())
+    });
+
+    it('run day1 - part 2', () => {
+        day2_part2();
+    });
+});
---
+++ src/day2/day2.ts
@@ -0,0 +1,65 @@
+import {join} from 'path';
+import {readFileLines} from "../file-reader";
+import {isMap} from "node:util/types";
+
+export function day2(): number {
+    let fullyQualifiedFileName = join(__dirname, 'day2.txt');
+    const parser = new ElfGameParser();
+    const checker = new ElfGamePossibilityChecker();
+    return readFileLines(fullyQualifiedFileName)
+        .map(line => parser.parse(line))
+        .filter(game => checker.checkGame(game))
+        .map(elfGame => elfGame.id)
+        .reduce((previousValue, currentValue) => previousValue +currentValue, 0)
+}
+
+export function day2_part2(): void {
+}
+
+class ElfGame {
+    id: number = 0
+    highestGreen: Number = 0
+    highestBlue: Number = 0
+    highestRed: Number = 0
+}
+
+export class ElfGameParser {
+    public parse(input: string): ElfGame {
+        let gameId: number = parseInt(input.split(" ")[1].split(":")[0])
+        let green = input
+            .split(":")[1]
+            .split(";")
+            .flatMap(revealing => revealing.split(",")
+                .filter(color => color.includes("green"))
+                .map(color => parseInt(color.trim().split(" ")[0])))
+            .reduce((previousValue, currentValue) => currentValue > previousValue ? currentValue : previousValue)
+        let blue = input
+            .split(":")[1]
+            .split(";")
+            .flatMap(revealing => revealing.split(",")
+                .filter(color => color.includes("blue"))
+                .map(color => parseInt(color.trim().split(" ")[0])))
+            .reduce((previousValue, currentValue) => currentValue > previousValue ? currentValue : previousValue)
+        let red = input
+            .split(":")[1]
+            .split(";")
+            .flatMap(revealing => revealing.split(",")
+                .filter(color => color.includes("red"))
+                .map(color => parseInt(color.trim().split(" ")[0])))
+            .reduce((previousValue, currentValue) => currentValue > previousValue ? currentValue : previousValue)
+
+        return { id: gameId, highestGreen: green, highestBlue: blue, highestRed: red};
+    }
+}
+
+export class ElfGamePossibilityChecker {
+    acceptableRed = 12
+acceptableGreen= 13
+acceptableBlue= 14
+    public checkGame(game: ElfGame): boolean {
+        return game.highestRed <= this.acceptableRed
+        && game.highestGreen <= this.acceptableGreen
+        && game.highestBlue <= this.acceptableBlue
+    }
+}
+
